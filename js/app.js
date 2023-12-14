38f952656f0da3182615e8f9e3809a8c14570a0c
Manticora <999manticora999@gmail.com>
2023-12-14 20:36:16 +0300

    feat: add const bet & payout

--- js/app.js
+++ js/app.js
@@ -1,2 +1,61 @@
 // roulette-calculator
 
+const minBet = 1;
+const maxBet = 100;
+const payoutRatios = {
+    numb: {
+        payout: 35,
+        position: 1
+    },
+    split: {
+        payout: 17,
+        position: 2
+    },
+    corner: {
+        payout: 8,
+        position: 4
+    },
+    street: {
+        payout: 11,
+        position: 3
+    },
+    six_line: {
+        payout: 5,
+        position: 6
+    }
+};
+console.log(payoutRatios.split.payout); // 17
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
