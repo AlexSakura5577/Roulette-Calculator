d9e73601991b55e93b1d26f59fac0512e83ee4f9
Manticora <999manticora999@gmail.com>
2024-02-08 20:19:59 +0300

    feat: add const odds-dozen-columns

--- pages/multi-complete/multi-complete.js
+++ pages/multi-complete/multi-complete.js
@@ -5,31 +5,24 @@
 // import { rouletteNumber } from "../main/app.js";
 import { chipsNeededForABet } from "../main/app.js";
 import { countPositions } from "../main/app.js";
+import { oddDozColum } from "../main/app.js";
+
+console.log(oddDozColum.dozen_1.length);
 
 // переменные:
 let selectedNumbers = [];
 let positions;
 let result;
 let coincidences;
-
 // вывод сообщения алерт:
 function showMessage() {
     alert(`выбранные номера: ${selectedNumbers} \nкол-во позиций: ${positions} \nлишних позиций: ${coincidences} \nитоговая ставка: ${result}`);
     location.reload();
     return;
 };
-
 // функция подсчёта пересечений комплитов:
 const nodeList = document.querySelectorAll(".radioBtn");
-console.log(nodeList);
-// console.log(nodeList.item[1]);
-// console.log(nodeList[5].checked);
-
-// radioBtn.onclick = function () {
-//     if (nodeList[2].checked === true) {
-//         nodeList[13].checked = true
-//     };
-// };
+// console.log(nodeList);
 
 calculate.onclick = function () {
     nodeList.forEach((item, index) => {
@@ -48,20 +41,20 @@ calculate.onclick = function () {
             nodeList[14].checked = true
             nodeList[15].checked = true
         };
-        if (nodeList[17].checked === true) {
-            nodeList[18].checked = true
-            nodeList[19].checked = true
-            nodeList[20].checked = true
-            nodeList[21].checked = true
-            nodeList[22].checked = true
-            nodeList[23].checked = true
-            nodeList[25].checked = true
-            nodeList[26].checked = true
-            nodeList[27].checked = true
-            nodeList[28].checked = true
-            nodeList[29].checked = true
-            nodeList[30].checked = true
-        };
+        // if (nodeList[17].checked === true) {
+        //     nodeList[18].checked = true
+        //     nodeList[19].checked = true
+        //     nodeList[20].checked = true
+        //     nodeList[21].checked = true
+        //     nodeList[22].checked = true
+        //     nodeList[23].checked = true
+        //     nodeList[25].checked = true
+        //     nodeList[26].checked = true
+        //     nodeList[27].checked = true
+        //     nodeList[28].checked = true
+        //     nodeList[29].checked = true
+        //     nodeList[30].checked = true
+        // };
 
         let element_id = item.id;
         // console.log(element_id);
