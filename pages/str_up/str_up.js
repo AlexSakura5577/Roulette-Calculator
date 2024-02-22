c0b34996173b4530e43b9912c956f68903411fe1
Manticora <999manticora999@gmail.com>
2024-02-22 20:04:01 +0300

    test: test localstorage and sessionstorage

--- pages/str_up/str_up.js
+++ pages/str_up/str_up.js
@@ -10,4 +10,4 @@ function showMessage() {
     alert(`localStorage_1: ${+localStorage.getItem('number')} \nlocalStorage_2: ${+localStorage.getItem('number2')} \nsessionStorage_1: ${+sessionStorage.getItem('number')} \nsessionStorage_2: ${+sessionStorage.getItem('number2')}`);
     return;
 };
-
+showMessage();
