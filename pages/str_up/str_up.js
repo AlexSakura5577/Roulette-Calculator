94cb910b8f142a343b9aa2433cc41a53a87eac1a
Manticora <999manticora999@gmail.com>
2024-02-22 22:07:04 +0300

    test: local and session storage successfully

--- pages/str_up/str_up.js
+++ pages/str_up/str_up.js
@@ -1,13 +1,21 @@
 
+import { payoutRatios } from "../main/app.js";
+localStorage.setItem('payoutRatios', JSON.stringify(payoutRatios));
+console.log(JSON.parse(localStorage.getItem('payoutRatios')));
+
+// функция реагирования на изменения в localStorage:
+window.onstorage = () => {
+    console.log(event);
+};
+
 console.log(+localStorage.getItem('number'));
 console.log(+localStorage.getItem('number2'));
 
 console.log(+sessionStorage.getItem('number'));
 console.log(+sessionStorage.getItem('number2'));
 
-
 function showMessage() {
     alert(`localStorage_1: ${+localStorage.getItem('number')} \nlocalStorage_2: ${+localStorage.getItem('number2')} \nsessionStorage_1: ${+sessionStorage.getItem('number')} \nsessionStorage_2: ${+sessionStorage.getItem('number2')}`);
     return;
 };
-showMessage();
+// showMessage();
