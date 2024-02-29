a3d379cb9f4bd87c0a17f55cdee22c7a9592d00c
Manticora <999manticora999@gmail.com>
2024-03-01 02:34:22 +0300

    feat: test button position function

--- pages/field/field.js
+++ pages/field/field.js
@@ -8,8 +8,26 @@ import { rouletteNumber } from "../main/app.js";
 // import { oddDozColum } from "../main/app.js";
 import { fullBets } from "../main/app.js";
 
-let split_0_1 = rouletteNumber[0].positions.split[0];
-// console.log(split_0_1);
+// let split_0_1 = rouletteNumber[0].positions.split[0];
+// let firstfour = rouletteNumber[0].positions.corner[0];
+let firstfour = document.getElementById("firstfour");
+function test() {
+    alert("first four")
+};
+firstfour.addEventListener("click", test);
+
+// получить список элементов по классу
+const nodeList = document.querySelectorAll(".pos");
+// console.log(nodeList);
+
+// получить текст из тега элемента
+let split_01 = document.getElementById("split01");
+let content = split_01.textContent;
+console.log(content);
+function test2() {
+    alert(`${content}`)
+};
+split_01.addEventListener("click", test2);
 
 
 
