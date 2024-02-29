4a98c9ec846dfc532baac66c17c588d439fad7ed
Manticora <999manticora999@gmail.com>
2024-03-01 00:05:40 +0300

    feat: add empty items for field

---
+++ pages/field/field.js
@@ -0,0 +1,31 @@
+// import "../main/app.js";
+// import { minBet } from "../main/app.js";
+// import { maxBet } from "../main/app.js";
+// import { payoutRatios } from "../main/app.js";
+import { rouletteNumber } from "../main/app.js";
+// import { chipsNeededForABet } from "../main/app.js";
+// import { countPositions } from "../main/app.js";
+// import { oddDozColum } from "../main/app.js";
+import { fullBets } from "../main/app.js";
+
+let split_0_1 = rouletteNumber[0].positions.split[0];
+// console.log(split_0_1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
