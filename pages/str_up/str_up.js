7f8ec147d72d82894333bac1acc7d053e096ab2c
Manticora <999manticora999@gmail.com>
2024-03-15 22:08:23 +0300

    fix: fix bags reload page

--- pages/str_up/str_up.js
+++ pages/str_up/str_up.js
@@ -6,8 +6,8 @@ console.log(+sessionStorage.getItem('number'));
 console.log(+sessionStorage.getItem('number2'));
 
 
-function showMessage() {
-    alert(`localStorage_1: ${+localStorage.getItem('number')} \nlocalStorage_2: ${+localStorage.getItem('number2')} \nsessionStorage_1: ${+sessionStorage.getItem('number')} \nsessionStorage_2: ${+sessionStorage.getItem('number2')}`);
-    return;
-};
-showMessage();
+// function showMessage() {
+//     alert(`localStorage_1: ${+localStorage.getItem('number')} \nlocalStorage_2: ${+localStorage.getItem('number2')} \nsessionStorage_1: ${+sessionStorage.getItem('number')} \nsessionStorage_2: ${+sessionStorage.getItem('number2')}`);
+//     return;
+// };
+// showMessage();
