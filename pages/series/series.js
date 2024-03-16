08a1d3f991561e203fbcb0b6c8835776d2f5a42d
Manticora <999manticora999@gmail.com>
2024-03-16 18:14:47 +0300

    feat: add input fields for series

--- pages/series/series.js
+++ pages/series/series.js
@@ -1,2 +1,24 @@
+// выбор минимума - максимума (и запись в localstorage)
+let maxBet = document.getElementById('minmax')
 
 
+// let minBet = 1;
+// let maxBet = 100;
+// localStorage.setItem('minBet', minBet);
+// localStorage.setItem('maxBet', maxBet);
+
+// кнопка Сброс (перезагружает вкладку)
+reset.onclick = function () {
+    location.reload();
+    return;
+}
+// кнопка Рассчитать
+calculate.onclick = function () {
+    let bet = +document.getElementById('bet').value;
+    console.log(bet);
+    // let completeBet = document.getElementById('complete').value;
+
+    // document.getElementById('info_2').innerHTML = `strUp  ${strUp}: ${chipsNeededForABet(strUp)} positions of ${completeBet}<br\/> sumBet: $${sumBet(strUp)}<br\/> payment: ${completePayment(strUp)} chips<br\/> totalPayment: $${totalPayment(strUp)}<br\/> ${info(strUp)}`;
+
+    return;
+};
