52e568c220b237c805ff1f962283b2a89a86c2a4
Manticora <999manticora999@gmail.com>
2024-03-22 21:57:08 +0300

    feat: add min-max in localstorage

--- pages/series/series.js
+++ pages/series/series.js
@@ -1,23 +1,72 @@
-// выбор минимума - максимума (и запись в localstorage)
-let maxBet = document.getElementById('minmax')
-
-
-// let minBet = 1;
-// let maxBet = 100;
-// localStorage.setItem('minBet', minBet);
-// localStorage.setItem('maxBet', maxBet);
-
-// кнопка Сброс (перезагружает вкладку)
+localStorage.setItem('minBet', 1);
+localStorage.setItem('maxBet', 100);
+// кнопка Сброс:
 reset.onclick = function () {
     location.reload();
+    localStorage.setItem('minBet', 1);
+    localStorage.setItem('maxBet', 100);
     return;
-}
-// кнопка Рассчитать
+};
+// клик по кнопке Рассчитать:
 calculate.onclick = function () {
-    let bet = +document.getElementById('bet').value;
-    console.log(bet);
-    // let completeBet = document.getElementById('complete').value;
+    // выбор минимума-максимума рулетки:
+    let minmax = document.getElementById('minmax').value; // выбираем элемент select minmax
+    let minBet = localStorage.getItem('minBet');
+    let maxBet = localStorage.getItem('maxBet');
+    switch (minmax) {
+        case "1-100":
+            minBet = 1;
+            maxBet = 100;
+            localStorage.setItem('minBet', 1);
+            localStorage.setItem('maxBet', 100);
+            break;
+        case "5-200":
+            minBet = 5;
+            maxBet = 200;
+            localStorage.setItem('minBet', 5);
+            localStorage.setItem('maxBet', 200);
+            break;
+        case "5-300":
+            minBet = 5;
+            maxBet = 300;
+            localStorage.setItem('minBet', 5);
+            localStorage.setItem('maxBet', 300);
+            break;
+        case "25-500":
+            minBet = 25;
+            maxBet = 500;
+            localStorage.setItem('minBet', 25);
+            localStorage.setItem('maxBet', 500);
+            break;
+        default:
+            minBet = 1;
+            maxBet = 100;
+            localStorage.setItem('minBet', 1);
+            localStorage.setItem('maxBet', 100);
+    };
+    console.log(`мин-макс: ${minmax}`);
+    // console.log(minBet);
+    // console.log(maxBet);
 
+    // выбор серии:
+    let series = document.getElementById('series').value;
+    console.log(`серия: ${series}`);
+
+    // введённая сумма ставки на серию:
+    let bet = +document.getElementById('bet').value;
+    console.log(`ставка: ${bet}`);
+
+    // вычисления:
+
+
+    // по чём играет:
+    let plays = 0;
+    console.log(`играет по: ${plays}`);
+
+    // сдача:
+    let residue = 0;
+    console.log(`сдача: ${residue}`);
+    
     // document.getElementById('info_2').innerHTML = `strUp  ${strUp}: ${chipsNeededForABet(strUp)} positions of ${completeBet}<br\/> sumBet: $${sumBet(strUp)}<br\/> payment: ${completePayment(strUp)} chips<br\/> totalPayment: $${totalPayment(strUp)}<br\/> ${info(strUp)}`;
 
     return;
