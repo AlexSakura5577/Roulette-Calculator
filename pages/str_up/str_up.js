6095f2f4f1fa96c8e4e3940f5bb1fdf8f2619a73
Manticora <999manticora999@gmail.com>
2024-03-28 23:35:49 +0300

    docs: create str-up branch

--- pages/str_up/str_up.js
+++ pages/str_up/str_up.js
@@ -1,13 +1,59 @@
+// импорты:
+// import "../main/app.js";
+import { rouletteSeries } from "../main/app.js";
+import { payoutRatios } from "../main/app.js";
 
-console.log(+localStorage.getItem('number'));
-console.log(+localStorage.getItem('number2'));
+// localStorage
+localStorage.setItem('minBet', 1);
+localStorage.setItem('maxBet', 100);
+// кнопка Сброс:
+reset.onclick = function () {
+    location.reload();
+    localStorage.setItem('minBet', 1);
+    localStorage.setItem('maxBet', 100);
+    return;
+};
 
-console.log(+sessionStorage.getItem('number'));
-console.log(+sessionStorage.getItem('number2'));
+// клик по кнопке Рассчитать:
+calculate.onclick = function () {
+    // выбор минимума-максимума рулетки:
+    let minmax = document.getElementById('minmax').value; // выбираем элемент select minmax
+    let minBet = localStorage.getItem('minBet');
+    let maxBet = localStorage.getItem('maxBet');
+    switch (minmax) {
+        case "1-100":
+            minBet = 1;
+            maxBet = 100;
+            localStorage.setItem('minBet', 1);
+            localStorage.setItem('maxBet', 100);
+            break;
+        case "5-200":
+            minBet = 5;
+            maxBet = 200;
+            localStorage.setItem('minBet', 5);
+            localStorage.setItem('maxBet', 200);
+            break;
+        case "5-300":
+            minBet = 5;
+            maxBet = 300;
+            localStorage.setItem('minBet', 5);
+            localStorage.setItem('maxBet', 300);
+            break;
+        case "25-500":
+            minBet = 25;
+            maxBet = 500;
+            localStorage.setItem('minBet', 25);
+            localStorage.setItem('maxBet', 500);
+            break;
+        default:
+            minBet = 1;
+            maxBet = 100;
+            localStorage.setItem('minBet', 1);
+            localStorage.setItem('maxBet', 100);
+    };
 
+    document.getElementById('info_3').innerHTML =
+    `шаг ломается от: ${let_1}<br\/> чистая ставка: ${let_2}<br\/> играет по:  ${let_3}<br\/> сдача: ${let_4}<br\/>`;
 
-// function showMessage() {
-//     alert(`localStorage_1: ${+localStorage.getItem('number')} \nlocalStorage_2: ${+localStorage.getItem('number2')} \nsessionStorage_1: ${+sessionStorage.getItem('number')} \nsessionStorage_2: ${+sessionStorage.getItem('number2')}`);
-//     return;
-// };
-// showMessage();
+    return;
+};
