04befaa2033800721df49edd38c05e31e2343ff6
Manticora <999manticora999@gmail.com>
2024-03-29 20:42:40 +0300

    test: test modal window

--- pages/main/app.js
+++ pages/main/app.js
@@ -1211,7 +1211,22 @@ localStorage.setItem('minBet', minBet);
 localStorage.setItem('maxBet', maxBet);
 localStorage.setItem('trackOverlapsField', trackOverlapsField);
 
+// открыть - закрыть модальное окно:
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const modal = document.getElementById('modal');
 
+openBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.showModal();
+});
+closeBtn.addEventListener('click', () => {
+    modal.close();
+});
+modal.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target === modal) modal.close();
+});
 
 
 
