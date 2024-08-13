f0687798a585ba124adc771fc8ea4d9254d1c683
Manticora <999manticora999@gmail.com>
2024-08-13 22:11:31 +0300

    docs: transfer strUpCalculations function to a separate module

--- roulette/models/str_upModel.js
+++ roulette/models/str_upModel.js
@@ -1,8 +1,8 @@
 import { minMax } from "../controllers/localStorageRead.js";
 import { updateMinMax } from "../controllers/updateMinMax.js";
 import { resetValues } from "../controllers/resetHandler.js";
-// import { payoutRatios } from "./consts/payoutRatios.js";
 import { chipCount } from "./functions/strUp/chipCount.js";
+import { strUpCalculations } from "./functions/strUp/strUpCalculations.js";
 
 // выбор минимума максимума рулетки:
 document.getElementById('minmax').addEventListener('change', function () {
@@ -20,11 +20,6 @@ const positions = document.getElementById('positions');
 const bet = document.getElementById('bet');
 const color = document.getElementById('color');
 const cash = document.getElementById('cash');
-let payChips = 0;
-let summPay = 0;
-let summCash = 0;
-let residue = 0;
-let pay = 0;
 
 // массив значений:
 let fillArr = [];
@@ -36,38 +31,9 @@ add_bet.onclick = function () {
 
 // клик по кнопке Рассчитать:
 calculate.onclick = function () {
-    // выплата (кол-во фишек):
-    pay = fillArr.reduce((acc, number) => acc + number, 0);
-    // console.log(`кол-во фишек: ${pay}`);
+    let infoObj = strUpCalculations(fillArr); // Вызываем strUpCalculations и сохраняем результат в infoObj
+    console.log(infoObj);
 
-    // цвет по:
-    console.log(`цвет по: ${color.value}`);
-
-    // сумма выплаты:
-    summPay = +color.value * pay;
-    console.log(`выплата: ${summPay}`);
-
-    // через сколько (кэш):
-    summCash = +cash.value;
-
-    // проверка кратности:
-    let colorCash = summCash / +color.value;
-    if ((colorCash ^ 0) !== colorCash) {
-        summCash = `${summCash} не получится`;
-        console.log("не получится");
-    } else {
-        console.log("ok");
-    };
-    // console.log(`через (кэш): ${summCash}`);
-
-    // остаток:
-    residue = summPay - +cash.value;
-    // console.log(`остаток: ${residue}`);
-
-    // выплата цветом:
-    payChips = residue / +color.value;
-    // console.log(`выплата цветом: ${payChips}`);
-
-    document.getElementById('info_3').innerHTML = ` сумма фишек: ${pay}<br\/> сумма выплаты: ${summPay}<br\/> выплата цветом: ${payChips}<br\/> через: ${summCash}<br\/>`;
-    return;
+    // вывод информации пользователю:
+    document.getElementById('info_3').innerHTML = ` Выплата<br\/> сумма фишек: ${infoObj.pay}<br\/> сумма выплаты: ${infoObj.summPay}<br\/> выплата цветом: ${infoObj.payChips}<br\/> через: ${infoObj.summCash}<br\/>`;
 };
