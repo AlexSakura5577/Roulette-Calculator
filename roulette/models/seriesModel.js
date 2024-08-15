284f0d85467c1a9c6edf9cf3e9d59d673c0499c7
Manticora <999manticora999@gmail.com>
2024-08-15 18:17:57 +0300

    refactor: update seriesModel functions

--- roulette/models/seriesModel.js
+++ roulette/models/seriesModel.js
@@ -16,24 +16,26 @@ reset.onclick = function () {
 
 // клик по кнопке Рассчитать:
 calculate.onclick = function () {
-    // выбор серии:
+    // Выбор серии и суммы ставки:
     let series = document.getElementById('series').value;
-    // сумма ставки на серию:
     let bet = +document.getElementById('bet').value;
-    // вычисления:
+
+    // Выполнение вычислений:
     let objVarSeriesCalc = seriesCalc(minMax.maxBet, series, bet);
 
+    // Установка значения betWithoutChange
+    objVarSeriesCalc.betWithoutChange = bet - objVarSeriesCalc.change;
+    
     // вывод информации пользователю:
-    if (series == "tier") {
-        objVarSeriesCalc.betWithoutChange = bet - objVarSeriesCalc.change;
-        objVarSeriesCalc.breakingStep = "в tier не ломается";
-        document.getElementById('info_2').innerHTML =
-            ` максимум на серию: ${objVarSeriesCalc.maxSeries}<br\/> шаг: ${objVarSeriesCalc.breakingStep}<br\/> чистая ставка: ${objVarSeriesCalc.betWithoutChange}<br\/> играет по:  ${objVarSeriesCalc.plays}<br\/> сдача: ${objVarSeriesCalc.change}<br\/>`;
-    } else if (series != "tier") {
-        objVarSeriesCalc.betWithoutChange = bet - objVarSeriesCalc.change;
-        document.getElementById('info_2').innerHTML =
-            ` максимум на серию: ${objVarSeriesCalc.maxSeries}<br\/> шаг ломается от: ${objVarSeriesCalc.breakingStep}<br\/> чистая ставка: ${objVarSeriesCalc.betWithoutChange}<br\/> играет по:  ${objVarSeriesCalc.plays}<br\/> сдача: ${objVarSeriesCalc.change}<br\/>`;
-    };
+    const breakingStepText = series === "tier" ? "шаг в tier не ломается" : `шаг ломается от: ${objVarSeriesCalc.breakingStep}`;
+
+    document.getElementById('info_2').innerHTML = `
+        максимум на серию: ${objVarSeriesCalc.maxSeries}<br/>
+        ${breakingStepText}<br/>
+        чистая ставка: ${objVarSeriesCalc.betWithoutChange}<br/>
+        играет по: ${objVarSeriesCalc.plays}<br/>
+        сдача: ${objVarSeriesCalc.change}<br/>
+    `;
 
     // значения переменных для справки в консоли:
     console.log(objVarSeriesCalc);
