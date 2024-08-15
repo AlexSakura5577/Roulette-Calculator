c4847693719caf5db7644042782458a2a2eed8b9
Manticora <999manticora999@gmail.com>
2024-08-15 15:54:34 +0300

    refactor: update afterBreakingStep functions

--- roulette/models/functions/series/afterBreakingStep.js
+++ roulette/models/functions/series/afterBreakingStep.js
@@ -3,21 +3,17 @@ import { rouletteSeries } from "../../consts/rouletteSeries.js";
 // рассчёт после ломаного шага:
 function afterBreakingStep(minMax, series, breakingStep, bet, residue) {
     console.log("функция afterBreakingStep");
-    let coefficient = rouletteSeries[series].coefficient;
-    bet = bet - breakingStep;
-    let diff_1 = bet / coefficient;
-    let diff_2 = bet / coefficient % 5;
-    let plays_1 = diff_1 - diff_2;
-    let cleanBet = plays_1 * coefficient;
-    let diff_3 = bet - cleanBet;
-    let plays; // Объявляем переменную plays
-    if (series == "voisins") {
-        plays = (minMax.maxBet * 1.5) + plays_1;
-    } else {
-        plays = +minMax.maxBet + plays_1;
-    }
-    console.log(plays);
-    let change = diff_3 + residue;
+
+    const coefficient = rouletteSeries[series].coefficient; // Используем const для константы
+    bet -= breakingStep; // Упрощаем запись
+    const diff_1 = bet / coefficient;
+    const plays_1 = diff_1 - (bet / coefficient % 5); // Объединяем расчёт diff_2
+    const cleanBet = plays_1 * coefficient;
+    const change = (bet - cleanBet) + residue; // Объединяем расчёт change
+    const plays = (series === "voisins") // Упрощаем условное присвоение plays
+        ? (minMax.maxBet * 1.5) + plays_1
+        : +minMax.maxBet + plays_1;
+
     return { cleanBet, plays, change };
 };
 export { afterBreakingStep };
