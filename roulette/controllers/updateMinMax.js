3899d65de82c7152e814ef31d98bd50c80711f78
Manticora <999manticora999@gmail.com>
2024-08-16 20:39:36 +0300

    feat: add more selection of roulette min and max

--- roulette/controllers/updateMinMax.js
+++ roulette/controllers/updateMinMax.js
@@ -2,6 +2,10 @@
 export function updateMinMax(value, minMax) {
     // выбираем элемент select minmax
     switch (value) {
+        case "1-50":
+            minMax.minBet = 1;
+            minMax.maxBet = 50;
+            break;
         case "1-100":
             minMax.minBet = 1;
             minMax.maxBet = 100;
@@ -17,6 +21,10 @@ export function updateMinMax(value, minMax) {
         case "25-500":
             minMax.minBet = 25;
             minMax.maxBet = 500;
+            break;
+        case "100-5000":
+            minMax.minBet = 100;
+            minMax.maxBet = 5000;
             break;
         default:
             console.warn("Некорректное значение для minmax");
